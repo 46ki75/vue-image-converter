09691f1484c8ef69d2d97addf235b85ac9d4f25e
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-05-06 02:50:40 +0000

    feat: replace HelloWorld component with Image component and remove unused files

---
+++ src/components/Image.vue
@@ -0,0 +1,13 @@
+<template>
+  <img :src="src" alt="user contents" />
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{ src: string }>(), {});
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
