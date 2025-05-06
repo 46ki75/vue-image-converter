f69bc1420ad032c8468fed26dc9fada004fb28c6
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-05-06 02:56:30 +0000

    chore: update Image component structure and styles for better layout

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -1,13 +1,22 @@
 <template>
-  <img :src="src" alt="user contents" />
+  <div class="container">
+    <div class="header"></div>
+    <img :src="src" alt="user contents" />
+  </div>
 </template>
 
 <script setup lang="ts">
 withDefaults(defineProps<{ src: string }>(), {});
 </script>
 
-<style scoped>
-.read-the-docs {
-  color: #888;
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 0;
+}
+
+.header {
+  background-color: rgba(gray, 0.1);
 }
 </style>
