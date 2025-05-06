61aa57afd180d64c0335274761603b92abec164e
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-05-06 03:23:45 +0000

    chore: clean up Image component code and improve readability

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -8,7 +8,12 @@
       <img class="image" :src="src" :alt="`${name} (user contents)`" />
     </div>
     <div class="control">
-      <ElmMdiIcon :d="mdiTrashCanOutline" size="1.25rem" color="#c56565" />
+      <ElmMdiIcon
+        class="icon"
+        :d="mdiTrashCanOutline"
+        size="1.25rem"
+        color="#c56565"
+      />
     </div>
   </div>
 </template>
@@ -22,6 +27,7 @@ withDefaults(defineProps<{ src: string; name: string }>(), {});
 
 <style scoped lang="scss">
 .container {
+  width: 16rem;
   padding: 0;
   margin: 0;
   border-radius: 0.25rem;
@@ -31,7 +37,7 @@ withDefaults(defineProps<{ src: string; name: string }>(), {});
 .header {
   box-sizing: border-box;
   background-color: rgba(gray, 0.1);
-  border-bottom: 1px solid rgba(gray, 0.1);
+  border-bottom: 1px solid rgba(gray, 0.3);
   width: 100%;
   padding: 0.25rem;
   display: flex;
@@ -41,6 +47,7 @@ withDefaults(defineProps<{ src: string; name: string }>(), {});
 }
 
 .image-container {
+  background-color: rgba(gray, 0.05);
   box-sizing: border-box;
   display: flex;
   justify-content: center;
@@ -50,6 +57,7 @@ withDefaults(defineProps<{ src: string; name: string }>(), {});
 .image {
   display: block;
   max-height: 12rem;
+  user-select: none;
 }
 
 .control {
@@ -57,10 +65,26 @@ withDefaults(defineProps<{ src: string; name: string }>(), {});
   width: 100%;
   padding: 0.25rem;
   background-color: rgba(gray, 0.1);
-  border-top: 1px solid rgba(gray, 0.1);
+  border-top: 1px solid rgba(gray, 0.3);
   display: flex;
   justify-content: flex-end;
   align-items: center;
   gap: 0.25rem;
+}
+
+.icon {
+  padding: 0.125rem;
+  border-radius: 0.125rem;
+  cursor: pointer;
+  transition: background-color 100ms, opacity 100ms, transform 100ms;
+
+  &:hover {
+    background-color: rgba(gray, 0.2);
+  }
+
+  &:active {
+    opacity: 0.5;
+    transform: translateX(1px) translateY(1px);
+  }
 }
 </style>
