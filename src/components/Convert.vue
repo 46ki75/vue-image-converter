9031be149e2b5d1d2f9d72754204038aef9050b7
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-05-06 04:26:22 +0000

    feat: enhance Convert component with button variants and loading state

--- src/components/Convert.vue
+++ src/components/Convert.vue
@@ -1,10 +1,34 @@
 <template>
   <ElmArrowIcon :loading="loading" />
-  <div></div>
+  <div class="container">
+    <ElmButton
+      v-for="variant in variants"
+      :key="variant"
+      block
+      :disabled="loading"
+      @click="() => {}"
+    >
+      <ElmMdiIcon :d="mdiImageSync" color="gray" size="1.25rem" />
+      <ElmInlineText :text="variant" />
+    </ElmButton>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ElmArrowIcon } from "@elmethis/core";
+import {
+  ElmArrowIcon,
+  ElmButton,
+  ElmInlineText,
+  ElmMdiIcon,
+} from "@elmethis/core";
+import { mdiImageSync } from "@mdi/js";
+
+const variants: Array<"BMP" | "JPEG" | "PNG" | "WEBP"> = [
+  "BMP",
+  "JPEG",
+  "PNG",
+  "WEBP",
+] as const;
 
 withDefaults(
   defineProps<{
@@ -15,3 +39,13 @@ withDefaults(
   }
 );
 </script>
+
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+}
+</style>
