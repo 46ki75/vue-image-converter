926c7a3bd58541bacc3b4a51c814b53ad457deb3
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-05-06 03:13:33 +0000

    feat: add Elmethis dependencies and enhance Image component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import Image from "./components/Image.vue";
 
 <template>
   <Image
+    name="untitled"
     src="https://avatars.githubusercontent.com/u/85323087?s=400&u=2867edf10c412da0ac9ddff6ff0581e92e79e563&v=4"
   />
 </template>
--- src/components/Image.vue
+++ src/components/Image.vue
@@ -1,22 +1,66 @@
 <template>
   <div class="container">
-    <div class="header"></div>
-    <img :src="src" alt="user contents" />
+    <div class="header">
+      <ElmMdiIcon :d="mdiImage" size="1.25rem" color="gray" />
+      <ElmInlineText :text="name" />
+    </div>
+    <div class="image-container">
+      <img class="image" :src="src" :alt="`${name} (user contents)`" />
+    </div>
+    <div class="control">
+      <ElmMdiIcon :d="mdiTrashCanOutline" size="1.25rem" color="#c56565" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-withDefaults(defineProps<{ src: string }>(), {});
+import { mdiImage, mdiTrashCanOutline } from "@mdi/js";
+import { ElmInlineText, ElmMdiIcon } from "@elmethis/core";
+
+withDefaults(defineProps<{ src: string; name: string }>(), {});
 </script>
 
 <style scoped lang="scss">
 .container {
-  display: flex;
-  flex-direction: column;
-  gap: 0;
+  padding: 0;
+  margin: 0;
+  border-radius: 0.25rem;
+  box-shadow: 0 0 0.125rem rgba(black, 0.3);
 }
 
 .header {
+  box-sizing: border-box;
   background-color: rgba(gray, 0.1);
+  border-bottom: 1px solid rgba(gray, 0.1);
+  width: 100%;
+  padding: 0.25rem;
+  display: flex;
+  justify-self: flex-start;
+  align-items: center;
+  gap: 0.25rem;
+}
+
+.image-container {
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.image {
+  display: block;
+  max-height: 12rem;
+}
+
+.control {
+  box-sizing: border-box;
+  width: 100%;
+  padding: 0.25rem;
+  background-color: rgba(gray, 0.1);
+  border-top: 1px solid rgba(gray, 0.1);
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 0.25rem;
 }
 </style>
