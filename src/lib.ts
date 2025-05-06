7641abab635e26e291a8463fcc29978a4875427b
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-05-06 14:59:28 +0000

    feat: enhance image converter module and update Vite configuration

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,14 @@
 import Application from "./components/Application.vue";
+import { api, convertImageWithWebWorker } from "./util";
+import { type Status, useImageConverter } from "./useImageConverter";
+import { type ImageFormat, IMAGE_FORMATS } from "./type";
 
-export { Application as ImageConverter };
+export {
+  Application as ImageConverter,
+  api,
+  convertImageWithWebWorker,
+  type Status,
+  useImageConverter,
+  type ImageFormat,
+  IMAGE_FORMATS,
+};
--- vite.config.build.ts
+++ vite.config.build.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 import { resolve } from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({ tsconfigPath: "./tsconfig.app.json" }),
+    cssInjectedByJsPlugin(),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/lib.ts"),
