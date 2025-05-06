6307a3731a3d3285c32caee25fab1e70eb2e9305
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-05-06 16:21:32 +0000

    refactor: remove imageConverterFunctionMap import from lib.ts

--- src/lib.ts
+++ src/lib.ts
@@ -1,11 +1,9 @@
 import Application from "./components/Application.vue";
-import { imageConverterFunctionMap } from "./examples/imageConverterFunctionMap";
 import { type Status, useImageConverter } from "./useImageConverter";
 import { type ImageFormat, IMAGE_FORMATS } from "./type";
 
 export {
   Application as ImageConverter,
-  imageConverterFunctionMap as api,
   type Status,
   useImageConverter,
   type ImageFormat,
